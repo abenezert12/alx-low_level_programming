1e26d8391ef85a5513a7233fb97b40db9b4bd309
Abenezer Tekalign <abenezertekalign12@gmail.com>
2022-12-11 15:15:06 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,12 +9,15 @@
 
 int main(void)
 {
+
 	int num1, num2;
 
 	for (num1 = 0; num1 <= 98; num1++)
 	{
+
 		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
+
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
 			putchar(' ');
@@ -31,6 +34,5 @@ int main(void)
 	}
 	
 	putchar('\n');
-
 	return (0);
 }
