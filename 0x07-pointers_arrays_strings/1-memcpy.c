71a4865d8a43245074757658bd5de11c09bb78e1
Abenezer Tekalign <117775495+abenezert12@users.noreply.github.com>
2023-01-03 01:41:15 +0300

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+/**
+ * file: 1-memcpy.c
+ * Auth: Abenezer
+
+#include "main.h"
+
+/**
+ * _memcpy - Entry point
+ * @dest: input
+ * @src: input
+ * @n: input
+ *
+ * Return: Always 0 (Success)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int index;
+	unsigned char *destination = dest;
+	const unsigned char *source = src;
+
+	for (index = 0; index < n; index++)
+		destination[index] = source[index];
+
+	return (dest);
+}
