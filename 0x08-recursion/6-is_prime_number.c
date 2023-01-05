351a981929bce369f19b0d019c29c86481e13b62
Abenezer Tekalign <117775495+abenezert12@users.noreply.github.com>
2023-01-05 06:38:35 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,20 +1,13 @@
-/*                                                                                                                                    
- * File: 6-is_prime_number.c                                                                                                          
- * Auth: sam tech                                                                                                                     
- */                                                                                                                                   
-                                                                                                                                      
-#include "main.h"                                                                                                                     
-                                                                                                                                      
-int is_divisible(int num, int div);                                                                                                   
-int is_prime_number(int n);                                                                                                           
-                                                                                                                                      
+#include "main.h"
+#include <stdio.h>
+                                                                                                                                     
 /**                                                                                                                                   
  * is_divisible - Checks if a number is divisible.                                                                                    
  * @num: The number to be checked.                                                                                                    
  * @div: The divisor.                                                                                                                 
  *                                                                                                                                    
  * Return: If the number is divisible - 0.                                                                                            
- *         If the number is not divisible - 1.                                                                                        
+ * If the number is not divisible - 1.                                                                                        
  */                                                                                                                                   
 int is_divisible(int num, int div)                                                                                                    
 {                                                                                                                                     
