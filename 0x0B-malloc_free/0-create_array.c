ca21c6a90f283f1f385d8d2c4ca61b2cc6998ec1
Abenezer Tekalign <117775495+abenezert12@users.noreply.github.com>
2023-01-09 22:35:46 +0300

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,50 +1,27 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
-#include "holberton.h"
+#include "main.h"
 
 /**
- *
- *  * create_array - main entry
- *
- *   * @size: size input
- *
- *    * @c: char
- *
- *     * Return: 0
- *
- *      */
-
+ * create_array - main entry
+ * @size: size input
+ * @c: char
+ * Return: 0
+ */
 char *create_array(unsigned int size, char c)
-
 {
 
-		char *array;
+	char *array;
+	unsigned int i;
 
-			unsigned int i;
+	array = malloc(sizeof(char) * size);
 
+	if (size == 0 || array == NULL)
+		return (NULL);
 
+	for (i = 0; i < size; i++)
+		array[i] = c;
 
-				array = malloc(sizeof(char) * size);
-
-
-
-					if (size == 0 || array == NULL)
-
-								return (NULL);
-
-
-
-						for (i = 0; i < size; i++)
-
-									array[i] = c;
-
-							return (array);
+	return (array);
 
 }
-
-Footer
-
-© 2023 GitHub, Inc.
-
