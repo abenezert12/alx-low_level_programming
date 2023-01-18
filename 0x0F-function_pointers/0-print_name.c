5026beb5bb861be791dbbeccf3f08a961998255a
Abenezer Tekalign <117775495+abenezert12@users.noreply.github.com>
2023-01-18 22:41:11 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,15 +2,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name - function to print name
- * @name: pointer of char type
- * @f: pointer to function
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
  * Return: Always successful
  */
-
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
-	if (f == NULL)
+	if (!name || !f)
 		return;
 	f(name);
 }
