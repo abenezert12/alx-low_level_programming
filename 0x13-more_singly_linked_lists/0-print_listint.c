22ced0dbff80407eb0c0bc50c8b8a19f474b6ec3
Abenezer Tekalign <117775495+abenezert12@users.noreply.github.com>
2023-01-30 21:59:22 +0300

    create 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,15 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
-	
+	size_t x = 0;
+
 	while (h)
 	{
-		nodes++;
 		printf("%d\n", h->n);
+
+		x++;
 		h = h->next;
 	}
-	
-	return (nodes);
+	return (x);
 }
-
