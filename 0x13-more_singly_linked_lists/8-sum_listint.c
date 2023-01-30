027348330c46291935cf388f56d55a56a45492bc
Abenezer Tekalign <117775495+abenezert12@users.noreply.github.com>
2023-01-30 22:26:23 +0300

    Create 8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * sum_listint - add new node
+ * @head: node head
+ * Description: Add a new node
+ * Return: New node
+ */
+int sum_listint(listint_t *head)
+{
+	unsigned int add = 0;
+
+	while (head)
+	{
+		add += head->n;
+		head = head->next;
+	}
+	return (add);
+}
