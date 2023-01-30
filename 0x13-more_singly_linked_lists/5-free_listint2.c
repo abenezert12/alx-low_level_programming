1bd1a3738bc34e88d691d7aa4cdefd74bea7a222
Abenezer Tekalign <117775495+abenezert12@users.noreply.github.com>
2023-01-30 22:21:54 +0300

    Create 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * free_listint2 - add new node
+ * @head: node head
+ * Description: Add a new node
+ * return: New node
+ */
+void free_listint2(listint_t **head)
+{
+	if (head && *head)
+	{
+		free_listint2(&(*head)->next);
+		free(*head);
+		*head = NULL;
+	}
+}
