939fd0242f785d69c0f3f8ef8b5c713c26a131a8
Abenezer Tekalign <117775495+abenezert12@users.noreply.github.com>
2023-01-30 22:35:03 +0300

    Create 100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * reverse_listint - reverse a node
+ * @head: node head
+ * Description: Add a new node
+ * Return: New node
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *first;
+	listint_t  *last;
+
+	if (!head && !*head)
+		return (NULL);
+
+	first = *head;
+	*head = NULL;
+	while (first)
+	{
+		last = first;
+		first = first->next;
+		last->next = *head;
+		*head = last;
+	}
+	return (*head);
+}
