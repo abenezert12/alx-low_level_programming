608480d302626674f8d889cdf524517649421943
Abenezer Tekalign <117775495+abenezert12@users.noreply.github.com>
2023-02-07 00:02:29 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
 /**
- * append_text_to_file - Function that creates a file
- * @filename: type char filename pointer of FD
- * @text_content: type char pointer of char
- * Return: -1 if NULL in FN, 1 on sucess
+ * append_text_to_file - appends text
+ * @filename: the file
+ * @text_content: string in the file
+ * Description: Function that append a file
+ * Return: -1 or 1
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file;
-	int final_out;
-	int x;
+	int fp = 0, w = -1;
 
-	file = open(filename, O_RDWR | O_APPEND);
-	if (filename == NULL)
-		return (-1);
+	if (filename)
+	{
+		for (w = 0; text_content && text_content[w]; w++)
+			;
+		fp = open(filename, O_WRONLY | O_APPEND);
 
-	if (file == -1)
-	return (-1);
+		if (text_content && fp != -1)
+		{
+			w = write(fp, text_content, w);
+			close(fp);
+		}
+		else
+			if (text_content)
+				w = -1;
+	}
 
-	if (text_content == NULL)
-		return (1);
-	while (*(text_content + x) != '\0')
-		x++;
-
-	final_out = write(file, text_content, x);
-	if (final_out == -1)
-		return (-1);
-	return (1);
+	return (w != -1 ? 1 : w);
 }
